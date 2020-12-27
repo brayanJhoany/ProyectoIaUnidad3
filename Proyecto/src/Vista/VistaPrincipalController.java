eb419f0779ca46c7fb160a644215e095ae37a914
brayan <brayan.escobar@live.com>
2020-12-26 21:28:55 -0300

    version estable sin validar entradas

--- Proyecto/src/Vista/VistaPrincipalController.java
+++ Proyecto/src/Vista/VistaPrincipalController.java
@@ -14,7 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import proyecto.Iris;
 import proyecto.Knn;
 import proyecto.Lector;
@@ -43,9 +42,11 @@ public class VistaPrincipalController implements Initializable {
     private TextArea mostrarInfo;
 
     public VistaPrincipalController() {
+        
         this.archivo_iris = new Lector("iris.txt");
-        this.irisDataset = archivo_iris.GetListaIris();
+        this.irisDataset =  archivo_iris.GetListaIris();
         this.irisKnn = new Knn();
+        System.out.println("Tam lista: "+ this.irisDataset.size() );
     }
 
     /**
@@ -58,7 +59,7 @@ public class VistaPrincipalController implements Initializable {
         //this.petalLenghtField = new TextField();
         //this.petalWidthField = new TextField();
         this.calcularBtn = new Button();
-        this.mostrarInfo = new TextArea();
+
 
     }
 
@@ -74,11 +75,12 @@ public class VistaPrincipalController implements Initializable {
             double valor2=Double.parseDouble(sepalWidth);
             double valor3=Double.parseDouble(petalLenght);
             double valor4=Double.parseDouble(petalWidth);
-            //Iris CasoDePrueba = new Iris(valor1,valor2,valor3,valor4,null);
-             Iris CasoDePrueba = new Iris(4.5,2.3,1.3,0.3,null);
+             Iris CasoDePrueba = new Iris(valor1,valor2,valor3,valor4,null);
              CasoDePrueba.setIrisType(irisKnn.getTipoIris(3, irisDataset, CasoDePrueba));
-            System.out.println(CasoDePrueba.toString());
-            //this.mostrarInfo.setText(CasoDePrueba.toString());
+             String res=CasoDePrueba.toString();
+            System.out.println(res);
+            this.mostrarInfo.clear();
+            this.mostrarInfo.appendText(res);
             return;
         }
         System.out.println("No hay info");
